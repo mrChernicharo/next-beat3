adceef4e34e846fa5c2445d93c01d683c6ab031e
Felipe Chernicharo <string7dev@gmail.com>
2021-02-14 11:32:36 -0300

    iniciando

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import style from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={style.main}>
+      <h1>Next Beat 3</h1>
+    </div>
+  );
+}
